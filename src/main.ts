ee7bba17379b358d4ccf7bb0ef06b8d351a81cbe
David McCloskey <davmcclo@gmail.com>
2021-01-21 14:08:02 -0800

    Updating code to match purge-artifacts-action

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import differenceInMilliseconds from 'date-fns/differenceInMilliseconds'
+import {
+  getOctokit,
+  eachWorkflowRun,
+  getActionInputs,
+  IActionInputs
+} from './utils'
+
+export function shouldDelete(
+  run: ActionsListWorkflowRunsForRepoResponseDataWorkflowRunsItem,
+  actionInputs: IActionInputs
+): boolean {
+  const expired =
+    differenceInMilliseconds(new Date(), new Date(run.created_at)) >=
+    actionInputs.expireInMs
+  return expired
+}
+
+export async function main(): Promise<void> {
+  try {
+    const actionInputs = getActionInputs()
+
+    const octokit = getOctokit()
+
+    const deletedRuns = []
+    for await (const run of eachWorkflowRun(octokit)) {
+      if (shouldDelete(run, actionInputs)) {
+        deletedRuns.push(run)
+        core.debug(`Deleting run:\n${JSON.stringify(run, null, 2)}`)
+        await octokit.actions.deleteWorkflowRunLogs({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          run_id: run.id
+        })
+      }
+    }
+    core.setOutput('deleted-runs', JSON.stringify(deletedRuns))
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+main()
